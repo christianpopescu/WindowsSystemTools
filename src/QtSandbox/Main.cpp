19106b5021b5ec8e3078998c5af37bd3958ac6f7
Christian Popescu <christian.popescu@outlook.com>
2020-12-13 18:48:23 +0100

    Windows with button maximized added

--- src/QtSandbox/Main.cpp
+++ src/QtSandbox/Main.cpp
@@ -1,12 +1,19 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QVBoxLayout>
 
 int main(int argc, char **argv)
 {
     QApplication app (argc, argv);
+    QWidget *window = new QWidget;
+    window->setWindowTitle("Processes");
+    
     QPushButton *button = new QPushButton("Quit");
     QObject::connect(button, SIGNAL(clicked()),
         &app, SLOT(quit()));
-    button->show();
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(button);
+    window->setLayout(layout);
+    window->showMaximized();
     return app.exec();
 }
